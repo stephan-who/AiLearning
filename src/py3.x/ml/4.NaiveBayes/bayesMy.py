6090d623b13d125bb50fa616899ec3f1c9923ecd
gongxianglin <xianglin@iscas.ac.cn>
2019-11-29 17:33:23 +0800

    add data/

---
+++ src/py3.x/ml/4.NaiveBayes/bayesMy.py
@@ -0,0 +1,231 @@
+import numpy as np
+
+def loadDataSet():
+    """
+    创建数据集
+    :return: 单词列表postingList, 所属类别classVec
+    """
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'], #[0,0,1,1,1......]
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]  # 1 is abusive, 0 not
+    return postingList, classVec
+
+def createVocabList(dataSet):
+    """
+    获取所有单词的集合
+    :param dataSet: 数据集
+    :return: 所有单词的集合(即不含重复元素的单词列表)
+    """
+    vocabSet = set([])  # create empty set
+    for document in dataSet:
+        # 操作符 | 用于求两个集合的并集
+        vocabSet = vocabSet | set(document)  # union of the two sets
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+    """
+    遍历查看该单词是否出现，出现该单词则将该单词置1
+    :param vocabList: 所有单词集合列表
+    :param inputSet: 输入数据集
+    :return: 匹配列表[0,1,0,1...]，其中 1与0 表示词汇表中的单词是否出现在输入的数据集中
+    """
+    # 创建一个和词汇表等长的向量，并将其元素都设置为0
+    returnVec = [0] * len(vocabList)# [0,0......]
+    # 遍历文档中的所有单词，如果出现了词汇表中的单词，则将输出的文档向量中的对应值设为1
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print("the word: %s is not in my Vocabulary!" % word)
+    return returnVec
+
+def _trainNB0(trainMatrix, trainCategory):
+    """
+    训练数据原版
+    :param trainMatrix: 文件单词矩阵 [[1,0,1,1,1....],[],[]...]
+    :param trainCategory: 文件对应的类别[0,1,1,0....]，列表长度等于单词矩阵数，其中的1代表对应的文件是侮辱性文件，0代表不是侮辱性矩阵
+    :return:
+    """
+    # 文件数
+    numTrainDocs = len(trainMatrix)
+    # 单词数
+    numWords = len(trainMatrix[0])
+    # 侮辱性文件的出现概率，即trainCategory中所有的1的个数，
+    # 代表的就是多少个侮辱性文件，与文件的总数相除就得到了侮辱性文件的出现概率
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    # 构造单词出现次数列表
+    p0Num = np.zeros(numWords) # [0,0,0,.....]
+    p1Num = np.zeros(numWords) # [0,0,0,.....]
+
+    # 整个数据集单词出现总数
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+        # 是否是侮辱性文件
+        if trainCategory[i] == 1:
+            # 如果是侮辱性文件，对侮辱性文件的向量进行加和
+            p1Num += trainMatrix[i] #[0,1,1,....] + [0,1,1,....]->[0,2,2,...]
+            # 对向量中的所有元素进行求和，也就是计算所有侮辱性文件中出现的单词总数
+            p1Denom += np.sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += np.sum(trainMatrix[i])
+    # 类别1，即侮辱性文档的[P(F1|C1),P(F2|C1),P(F3|C1),P(F4|C1),P(F5|C1)....]列表
+    # 即 在1类别下，每个单词出现的概率
+    p1Vect = p1Num / p1Denom# [1,2,3,5]/90->[1/90,...]
+    # 类别0，即正常文档的[P(F1|C0),P(F2|C0),P(F3|C0),P(F4|C0),P(F5|C0)....]列表
+    # 即 在0类别下，每个单词出现的概率
+    p0Vect = p0Num / p0Denom
+    return p0Vect, p1Vect, pAbusive
+
+def trainNB0(trainMatrix, trainCategory):
+    """
+    训练数据优化版本
+    :param trainMatrix: 文件单词矩阵
+    :param trainCategory: 文件对应的类别
+    :return:
+    """
+    # 总文件数
+    numTrainDocs = len(trainMatrix)
+    # 总单词数
+    numWords = len(trainMatrix[0])
+    # 侮辱性文件的出现概率
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    # 构造单词出现次数列表
+    # p0Num 正常的统计
+    # p1Num 侮辱的统计
+    p0Num = np.ones(numWords)#[0,0......]->[1,1,1,1,1.....]
+    p1Num = np.ones(numWords)
+
+    # 整个数据集单词出现总数，2.0根据样本/实际调查结果调整分母的值（2主要是避免分母为0，当然值可以调整）
+    # p0Denom 正常的统计
+    # p1Denom 侮辱的统计
+    p0Denom = 2.0
+    p1Denom = 2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            # 累加辱骂词的频次
+            p1Num += trainMatrix[i]
+            # 对每篇文章的辱骂的频次 进行统计汇总
+            p1Denom += np.sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += np.sum(trainMatrix[i])
+    # 类别1，即侮辱性文档的[log(P(F1|C1)),log(P(F2|C1)),log(P(F3|C1)),log(P(F4|C1)),log(P(F5|C1))....]列表
+    p1Vect = np.log(p1Num / p1Denom)
+    # 类别0，即正常文档的[log(P(F1|C0)),log(P(F2|C0)),log(P(F3|C0)),log(P(F4|C0)),log(P(F5|C0))....]列表
+    p0Vect = np.log(p0Num / p0Denom)
+    return p0Vect, p1Vect, pAbusive
+
+
+def _train_naive_bayes(train_mat, train_category):
+    """
+    训练数据原版
+    :param trainMatrix:文件单词矩阵
+    :param trainCategory: 文件对应的类别
+    :return:
+    """
+    # 文档数目
+    train_doc_num = len(train_mat)
+    # 所有单词数据库的长度相同
+    words_num = len(train_mat[0])
+
+    pos_abusive = np.sum(train_category) / train_doc_num
+    # 单词出现次数
+    p0num = np.zeros(words_num)
+    p1num = np.zeros(words_num)
+    # 整个数据集单词出现的次数
+    p0num_all = 0
+    p1num_all = 0
+
+    for i in range(train_doc_num):
+        if train_category[i] == 1:
+            p1num += train_mat[i]             # 将侮辱性文件中的向量进行加和
+            p1num_all += np.sum(train_mat[i]) # 所有侮辱性文件中出现的单词总数
+        else:
+            p0num += train_mat[i]
+            p0num_all += np.sum(train_mat[i])
+
+    p1vec = p1num / p1num_all
+    p0vec = p0num / p0num_all
+    return p0vec, p1vec, pos_abusive
+
+def train_naive_bayes(train_mat, train_category):
+    """
+
+    :param train_mat:
+    :param train_category:
+    :return:
+    """
+    train_doc_num = len(train_mat)
+    words_num = len(train_mat[0])
+
+    pos_abusive = np.sum(train_category) / train_doc_num
+
+    p0num = np.ones(words_num)
+    p1num = np.ones(words_num)
+
+    p0num_all = 2.0
+    p1num_all = 2.0
+
+    for i in range(train_doc_num):
+        if train_category[i] == 1:
+            p1num += train_mat[i]
+            p1num_all += np.sum(train_mat[i])
+        else:
+            p0num_all += train_mat[i]
+            p1num_all += np.sum(train_mat[i])
+
+    p1vec = np.log(p1num / p1num_all)
+    p0vec = np.log(p0num / p0num_all)
+    return p0vec, p1vec, pos_abusive
+
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    """
+    P(C|F1F2...Fn) = P(F1F2...FN|C)P(C)/P(F1F2...Fn)
+    P(F1|C)*P(F2|C)....P(Fn|C)P(C) -> Log(P(F1|C)) + LOG(P(F2|C))+...+log(P(Fn|C))+log(P(C))
+    :param vec2Classify:待分类数据，向量
+    :param p0Vec:类别0
+    :param p1Vec:类别1侮辱性文档的列表[]
+    :param pCLass1:类别1， 侮辱性文件的出现概率
+    :return:
+    """
+    p1 = np.sum(vec2Classify * p1Vec) + np.log(pClass1) # 贝叶斯准则的分子
+    p0 = np.sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1) # 贝叶斯准则的分子
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+def testingNB():
+    """
+    :return:
+    """
+    listOPosts, listClasses = loadDataSet()
+
+    myVocabList= createVocabList(listOPosts)
+
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+
+    p0V, p1V, pAb = trainNB0(np.array(trainMat), np.array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classed af :', classifyNB(thisDoc, p0V, p1V, pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classed as :', classifyNB(thisDoc, p0V, p1V, pAb))
+
+
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'\W+', bigString)
+    return[tok.lower() for tok in listOfTokens if len(tok) > 2]
+
