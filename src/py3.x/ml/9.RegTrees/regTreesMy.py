55e4520c8ec1df5dc379adb4bc5a6d034ce6a2b3
gongxianglin <xianglin@iscas.ac.cn>
2019-12-11 16:08:42 +0800

    add data for decisionTree; add regTree of Chapter 9;

---
+++ src/py3.x/ml/9.RegTrees/regTreesMy.py
@@ -0,0 +1,62 @@
+from numpy import *
+
+def loatDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = [float(x) for x in curLine]
+        dataMat.append(fltLine)
+    return dataMat
+
+def binSplitDataSet(dataSet, feature, value):
+    mat0 = dataSet[nonzero(dataSet[:, feature] <= value)[0], :]
+    mat1 = dataSet[nonzero(dataSet[:, feature] > value)[0], :] # [0] 返回的是当前行号
+    return mat0, mat1
+
+def regLeaf(dataSet):
+    return mean(dataSet[:, -1])
+
+def regErr(dataSet):
+    return var(dataSet[:, -1]) * shape(dataSet)[0]
+
+def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
+    """
+
+    :param dataSet:
+    :param leafType: 建立叶子点的函数
+    :param errType:  误差计算函数（求总方差）
+    :param ops: [容许误差下降值， 切分的最少样本数]
+    :return: index
+    """
+    tolS = ops[0]
+    tolN = ops[1]
+    if len(set(dataSet[:, -1].T.tolist()[0])) == 1:
+        return None, leafType(dataSet)
+    m, n = shape(dataSet)
+    S = errType(dataSet)
+    bestS, bestIndex, bestValue = inf, 0, 0
+
+    for featIndex in range(n-1):
+        for splitVal in set(dataSet[:, featIndex].T.tolist()[0]):
+            mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
+            if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
+                continue
+            newS = errType(mat0) + errType(mat1)
+            if newS < bestS:
+                bestIndex = featIndex
+                bestValue = splitVal
+                bestS = newS
+    if (S - bestS) < tolS:
+        return None, leafType(dataSet)
+    mat0, mat1 = binSplitDataSet(dataSet, bestIndex, bestValue)
+    if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN): #当最佳划分后，集合过小，也不划分，产生叶节点
+        return None, leafType(dataSet)
+    return bestIndex, bestValue
+
+
+def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
+    feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
+    return retTree
+
+
